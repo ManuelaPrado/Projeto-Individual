f9a7598fede495bd0d4cb92312e65e15690f775d
ManuelaPrado <manuela.garcia@sptech.school>
2025-11-30 21:08:46 -0300

    Subindo atualização do banco

--- API/web-data-viz/src/database/script-tabelas.sql
+++ API/web-data-viz/src/database/script-tabelas.sql
@@ -6,6 +6,7 @@ CREATE TABLE usuario (
   idusuario int PRIMARY KEY auto_increment,
   nome VARCHAR(100) NOT NULL,
   email VARCHAR(150) NOT NULL UNIQUE,
+ check (email like ('%@%.%')),
   senha CHAR(10) NOT NULL
 );
 
@@ -13,22 +14,39 @@ select *from usuario;
 
 CREATE TABLE pontuacao (
     id_pontuacao INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT,
-    acertou TINYINT(1),                 -- 1 = acerto, 0 = erro
-    quiz INT,                           -- qual quiz foi respondido
+	pontuacao int,                 
+    tentativas int,                        
     data_resposta DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id_usuario INT,
     FOREIGN KEY (id_usuario) REFERENCES usuario(idusuario)
 );
 
+/*
+               SELECT 
+  pontuacao,
+   5- pontuacao  as respostas_erradas,
+   count(*)  tentativas
+FROM pontuacao 
+WHERE id_usuario = 2
+group by tentativas, pontuacao;
 
-insert into usuario (nome,email, senha) value
-('bubu', 'bubu@bubu', 'bubu');
+        SELECT 
+  pontuacao as Respostas_certa,
+    ROUND((pontuacao / 5) * 100, 2) AS porcentagem_acertos,
+    (SELECT COUNT(*) FROM pontuacao WHERE id_usuario = 2) AS quantidade_tentativas
+FROM pontuacao
+WHERE id_usuario =2
+    ;
 
-insert into pontuacao (id_usuario, acertou) value
-(1,0);
+               SELECT 
+  pontuacao as Respostas_certa,
+   5 - pontuacao as Repostas_erradas,
+    data_resposta
+FROM pontuacao
+WHERE id_usuario = 2
+    ;
 
-
-SELECT 
+/*SELECT 
     u.nome,
     SUM(p.acertou) AS acertos,
     COUNT(*) - SUM(p.acertou) AS erros,
@@ -42,4 +60,4 @@ from pontuacao p
 join usuario u 
 on u.idusuario = p.id_usuario
 GROUP BY u.idusuario, data_resposta;
-   
+   */  
